4381109cb75fa66f20f9c486420a3da6c045bef3
somoo1995 <86731953+somoo1995@users.noreply.github.com>
2023-11-07 10:49:09 +0900

    231107 - seonggwon - bookinglist 페이지 테스트

---
+++ CampingEveryShare/src/main/java/web/controller/BookingController.java
@@ -0,0 +1,20 @@
+package web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//어성권
+
+@Controller
+@RequestMapping("/booking")
+public class BookingController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@GetMapping("/list")
+	public void bookinglist() {
+		logger.info("하이?");
+	}
+}
---
+++ CampingEveryShare/src/main/webapp/WEB-INF/views/booking/list.jsp
@@ -0,0 +1,21 @@
+<%@page import="java.util.Date"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%> 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<c:import url="../layout/header.jsp" />
+
+<div class="container">
+
+<div id="pageTitle">
+<h1></h1>
+<hr>
+</div>
+
+
+
+
+</div><!-- .container -->
+
+<c:import url="../layout/footer.jsp" />
