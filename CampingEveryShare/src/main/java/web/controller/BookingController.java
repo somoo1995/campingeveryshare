2f5c9abeaae10eee65f58ed308447b7f9cff30d3
somoo1995 <86731953+somoo1995@users.noreply.github.com>
2023-11-08 10:27:09 +0900

    231108 - seonggwon - 북킹컨트롤러 작업중

--- CampingEveryShare/src/main/java/web/controller/BookingController.java
+++ CampingEveryShare/src/main/java/web/controller/BookingController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,7 +15,7 @@ public class BookingController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/list")
-	public void bookinglist() {
+	public void bookinglist(Model model) {
 		logger.info("하이?");
 	}
 }
