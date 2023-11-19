aecbe610faf772b3423a679bc849c2bbb9d3202c
Dain Kang <Dain Kang@DESKTOP-OQRSULT>
2023-11-19 22:18:01 +0900

    231119 - dain - pom.xml gson 추가 / RentService 누락 추가 / Car DTO 수정

--- CampingEveryShare/src/main/java/web/service/face/RentService.java
+++ CampingEveryShare/src/main/java/web/service/face/RentService.java
@@ -1,9 +1,19 @@
 package web.service.face;
 
+import java.util.List;
+
+import web.dto.Car;
+import web.dto.Rent;
 import web.util.Paging;
 
 public interface RentService {
 
 	public Paging getPaging(Paging param);
+
+	public List<Car> getCarList(Paging paging);
+
+	public Car view(Car car);
+
+	public List<Rent> getRentList(Car car);
 
 }
